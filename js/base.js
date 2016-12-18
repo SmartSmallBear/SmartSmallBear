5f7ac6a461553a8cd33bafe819ed6f2585183317
Aang <565253795@qq.com>
2016-12-18 18:54:25 +0800

    DemoSimple

---
+++ js/base.js
@@ -0,0 +1,23 @@
+document.getElementsByTagName('header')[0].innerHTML = 
+			'<a href="#"><img class="logo" src="../../img/logo/aa.jpg"/></a>'
+			+'<nav class="mainNav">'
+				+'<ul class="clearfix verticalCenter">'
+					+'<li><a href="#">首页</a></li>'
+					+'<li><a href="#">DEMO</a></li>'
+					+'<li><a href="#">项目作品</a></li>'
+					+'<li><a href="#">个人简介</a></li>'
+					+'<li><a href="#">其他</a></li>'
+				+'</ul>'
+			+'</nav>';
+
+document.getElementsByTagName('footer')[0].innerHTML = 
+			'<div>'
+				+'<span class="shares">'
+				+'<a href="#"></a>'
+				+'<a href="#"></a>'
+				+'<a href="#"></a>'
+				+'</span>'
+			+'</div>'
+			+'<div class="copyRight">'
+				+'<span>copyright&copy;版权没有，随意盗版<br/> 川ICP备594110号-1</span>'
+			+'</div>';
